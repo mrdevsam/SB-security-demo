c421263e62057504d5f7169631a33bb3ce7f57c2
mrdevsam <mdshoykotislam05@gmail.com>
2023-06-07 01:38:56 +0000

    adding simple crud rest api for product

--- src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
+++ src/main/java/com/example/springsecuritydemo/SpringSecurityDemoApplication.java
@@ -3,6 +3,12 @@ package com.example.springsecuritydemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.*;
+import java.util.*;
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
@@ -16,7 +22,6 @@ public class SpringSecurityDemoApplication {
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 class UserInfo{
 
@@ -32,7 +37,6 @@ class UserInfo{
 
 @Entity
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 class Product{
 
@@ -41,7 +45,72 @@ class Product{
 	private int id;
 	
 	private String name;
-	private int qnty;
+	private int quantity;
 	private double price;
+
+	public Product(String name, int quantity, double price) {
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
 }
 
+record ProductMask(String name, int quantity, double price) {}
+
+// repository layer
+
+interface UserInfoRepo extends JpaRepository<UserInfo, Integer> {
+	Optional<UserInfo> findByName(String username);
+}
+
+interface ProductRepo extends JpaRepository<Product, Integer> {
+}
+
+// controller layer
+
+@RestController
+@RequestMapping("/api/products")
+class ProductController {
+
+	private final ProductRepo repo;
+
+	public ProductController(ProductRepo repo) {
+		this.repo = repo;
+	}
+
+	@GetMapping("/all")
+	public List<Product> allProducts() {
+		return repo.findAll();
+	}
+
+	@GetMapping("/find/{id}")
+	public Product findAProduct(@PathVariable Integer id) {
+		return repo.findById(id).orElseThrow( () -> new RuntimeException("product not found") );
+	}
+
+	@PostMapping("/new")
+	public Product createProduct(@RequestBody ProductMask product) {
+		if(product.name() != null) {
+			Product pdt = new Product(product.name(), product.quantity(), product.price());
+			return repo.save(pdt);
+		}else {
+			throw new RuntimeException("null values are not allowed!!!");
+		}
+	}
+
+	@PutMapping("/update/{id}")
+	public Product updateProduct(@PathVariable Integer id, @RequestBody ProductMask product) {
+		Product newPdct = repo.findById(id).orElseThrow( () -> new RuntimeException("product not found") );
+
+		newPdct.setName(product.name());
+		newPdct.setQuantity(product.quantity());
+		newPdct.setPrice(product.price());
+
+		return repo.save(newPdct);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteProduct(@PathVariable Integer id) {
+		repo.deleteById(id);
+	}
+}
